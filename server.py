316ab2980b86eaa0bad866fdfa9e3c40e89c51c7
kehman18 <kehindeadekola96@gmail.com>
2024-09-28 12:04:56 +0100

    added the secret keys into an encoded files

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request, redirect, flash
 from flask_mail import Mail, Message
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 import re
 
 app = Flask(__name__)
-# load_dotenv()
-app.secret_key = 'adekolakehindepelumi' #os.getenv('app_secret_key')
+load_dotenv()
+app.secret_key = os.getenv('app_secret_key')
 
 # Email configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-#load_dotenv()
-app.config['MAIL_USERNAME'] = 'kehindeadekola96@gmail.com' #os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = 'vgfoqejwlhijkftm' #os.getenv('MAIL_PASSWORD')
+load_dotenv()
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 mail = Mail(app)
 
