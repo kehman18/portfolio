6dc8c1a8d2195a9d59ea40837f9475bf6af814d7
kehman18 <kehindeadekola96@gmail.com>
2024-11-21 09:46:11 +0100

    added a simple animation

--- static/script.js
+++ static/script.js
@@ -1,1 +1,21 @@
-console.log('hiiiii!')
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll(
+    "#mainImage, #projectTitle, .card-container p, #secondaryImagesContainer img"
+  );
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.style.animationPlayState = "running";
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  elements.forEach((el) => {
+    el.style.animationPlayState = "paused"; // Pause animations initially
+    observer.observe(el);
+  });
+});
